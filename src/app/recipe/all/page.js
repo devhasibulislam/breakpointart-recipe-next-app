1c10f184a9598a339a8f5d4a7a288d572d029535
devhasibulislam <devhasibulislam@gmail.com>
2024-01-17 02:22:57 +0600

    list page that point edit | view | delete purpose

---
+++ src/app/recipe/all/page.js
@@ -0,0 +1,28 @@
+/**
+ * Title: Write a program using JavaScript on Page
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 16, January 2024
+ */
+
+import Main from "@/app/layouts/Main";
+import AllRecipes from "@/components/AllRecipes";
+import React from "react";
+
+const Page = () => {
+  return (
+    <Main>
+      <AllRecipes />
+    </Main>
+  );
+};
+
+export default Page;
